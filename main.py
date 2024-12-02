71107cd36ab34617bbfd32ee942988799b6a4769
ST <stonia.thorand@gmail.com>
2024-12-02 10:13:29 +0100

    init faunc call

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from kyu8.is_the_string_uppercase import kyu8_is_uppercase
 
 def main():
-    kyu8_is_uppercase()
+    kyu8_is_uppercase('+H%)O9Z`JHPNQ4M*JK_@8N&{W 05\\SAYBV"')
+    #print('l')
+    
+if __name__ == "__main__":
+    main()
