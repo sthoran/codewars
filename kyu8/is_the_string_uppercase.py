4f597f7c6ede36786da9e057d094b015811f83b2
ST <stonia.thorand@gmail.com>
2024-12-02 09:13:56 +0100

    implement function

--- kyu8/is_the_string_uppercase.py
+++ kyu8/is_the_string_uppercase.py
@@ -1,2 +1,11 @@
-def kyu8_is_uppercase():
-    pass
+import string
+
+def kyu8_is_uppercase(inp):
+    special_char=string.punctuation
+    for i in inp:
+        if i in string.punctuation and i.isupper():
+            return True
+        if inp.isupper():
+            return True
+    else:
+        return False
