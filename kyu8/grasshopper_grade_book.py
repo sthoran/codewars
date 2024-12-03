61201ec10ce12140768e88770724959ae08f22b4
ST <stonia.thorand@gmail.com>
2024-12-03 11:06:08 +0100

    complete function

--- kyu8/grasshopper_grade_book.py
+++ kyu8/grasshopper_grade_book.py
@@ -1,8 +1,14 @@
 import statistics
 
 def get_grade(s1,s2,s3):
-    mean_grade= statistics.mean(s1,s2,s3)
-    if 90 <= mean_grade <= 100:
-        return 'A'
-    else:
-        return 'works' 
+    d = {
+        'F': [0,59],
+        'D': [60,69],
+        'C': [70,79],
+        'B':[80,89],
+        'A':[90,100]
+    }
+    mean_grade= (s1+s2+s3)/3
+    for key, value in d.items():
+        if value[0] <= int(mean_grade) <= value[1]:
+            return key 
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from kyu8.is_the_string_uppercase import kyu8_is_uppercase
-from grasshopper_grade_book import get_grade
+from kyu8.grasshopper_grade_book import get_grade
 
 def main():
     kyu8_is_uppercase('+H%)O9Z`JHPNQ4M*JK_@8N&{W 05\\SAYBV"')
     #print('l')
-    get_grade()
-    
+    get_grade(94,90,93)
+    print('l')
 if __name__ == "__main__":
     main()
