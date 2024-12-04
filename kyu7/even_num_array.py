5cbc5e9962695cb1455ab5682301bde1af9d5356
ST <stonia.thorand@gmail.com>
2024-12-04 09:55:22 +0100

    make the code cleaner

--- kyu7/even_num_array.py
+++ kyu7/even_num_array.py
@@ -2,4 +2,3 @@ def even_numbers(arr,n):
     even = [i for i in arr if i%2 == 0]
     return even[-n:]
             
-print('test')
--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from kyu7.even_num_array import even_numbers
 
 def main():
     kyu8_is_uppercase('+H%)O9Z`JHPNQ4M*JK_@8N&{W 05\\SAYBV"')
-    #print('l')
     get_grade(94,90,93)
     even_numbers(([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), [4, 6, 8])
-    print('l')
+    
 if __name__ == "__main__":
     main()
