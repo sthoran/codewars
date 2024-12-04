bedad971bc5bb5fa09fef6e701914898f8e0ced9
ST <stonia.thorand@gmail.com>
2024-12-04 10:45:10 +0100

    increment current status

--- kyu7/increment.py
+++ kyu7/increment.py
@@ -1,2 +1,15 @@
-def increment():
-    pass
+l =[]
+def incrementer(nums):
+    for i, idx in enumerate(nums):
+        print(idx)
+        arr = i+idx
+        for d in arr:
+            if d <= 10:
+                return 
+            
+        return i+idx
+        
+        #if 10 <= nums[i]+i <= 99:
+            #l.append(nums[i]+i[2])
+            
+    
