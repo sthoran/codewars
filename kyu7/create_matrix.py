c088fc4add68f9d40d5dab20354b3a86e05061ed
ST <stonia.thorand@gmail.com>
2024-12-04 10:12:24 +0100

    temp solution with np

---
+++ kyu7/create_matrix.py
@@ -0,0 +1,4 @@
+import numpy as np 
+
+def get_matrix(n):
+    return np.eye(n)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from kyu8.is_the_string_uppercase import kyu8_is_uppercase
 from kyu8.grasshopper_grade_book import get_grade
 from kyu7.even_num_array import even_numbers
+from kyu7.create_matrix import get_matrix
 
 def main():
     kyu8_is_uppercase('+H%)O9Z`JHPNQ4M*JK_@8N&{W 05\\SAYBV"')
     get_grade(94,90,93)
     even_numbers(([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), [4, 6, 8])
+    get_matrix(5)
     
 if __name__ == "__main__":
     main()
